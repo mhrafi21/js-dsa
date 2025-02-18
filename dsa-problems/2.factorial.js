a2340c6357cd193c85181b157589f0a86c510fb7
mhrafi21 <12mhrafi@gmail.com>
2025-02-19 00:16:23 +0600

    updated

--- dsa-problems/2.factorial.js
+++ dsa-problems/2.factorial.js
@@ -12,7 +12,7 @@ factorial(4) = 4 * 3 * 2 * 1
 */
 
 function factorial(n){
-    const result = 1;
+    let result = 1;
     for(let i = 2; i <= n; i++){
         result = result * i
     }
@@ -22,3 +22,14 @@ function factorial(n){
 console.log(factorial(0)) // output: 1
 console.log(factorial(1)) // output: 1
 console.log(factorial(5)) // output: 120
+
+
+/*
+    Big-o Guide 
+    ------------
+    Calculation not dependent on input size - 0(1)
+    1 loop - O(n)
+    2 nested loops - O(n power 2)
+    input size reduced by half - O(logn)
+    
+*/
---
+++ dsa-problems/3.prime.js
@@ -0,0 +1,33 @@
+// problem: Give a natural number 'n', determine if the number is prime or not.Give
+// A prime number is a natural number greater than 1 that is not a product of two smaller natural numbers
+
+// isPrime(5) = true(1*5)
+// isPrime(4) = false
+
+function isPrime(n){
+    if(n < 2){
+        return false
+    }
+    for(let i =2; i < n; i++){
+        if(n % i === 0){
+            return false;
+        }
+    }
+    return true
+}
+
+function isPrime(n){
+    if(n < 2){
+        return false
+    }
+    for(let i =2; i <= Math.sqrt(n); i++){
+        if(n % i === 0){
+            return false;
+        }
+    }
+    return true
+}
+
+console.log(isPrime(1)) // output: false
+console.log(isPrime(5)) // output: true
+console.log(isPrime(4)) // output: false
